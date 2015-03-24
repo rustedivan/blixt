0eda6737525a4990b7f61637d4e7e61c74459824
Ivan Milles <ivan@socialthrills.se>
2015-03-24 08:20:22 +0100

    Initial commit.

---
+++ blixtTests/blixtTests.swift
@@ -0,0 +1,36 @@
+//
+//  blixtTests.swift
+//  blixtTests
+//
+//  Created by Ivan Milles on 24/03/15.
+//  Copyright (c) 2015 Rusted. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class blixtTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
